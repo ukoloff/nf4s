e8eaeafdef8c4ab51bc1c38f67221607b9f549a9
Stas Ukolov <ukoloff@gmail.com>
2016-11-18 14:46:18 +0500

    Tabs 2 spaces 2

--- nf4s/nf4s.cpp
+++ nf4s/nf4s.cpp
@@ -10,23 +10,23 @@ using namespace std;
 
 void readDbs(string name)
 {
-	ifstream dbs;
-	dbs.exceptions(ifstream::failbit | ifstream::eofbit | ifstream::badbit);
-	dbs.open(name);
+    ifstream dbs;
+    dbs.exceptions(ifstream::failbit | ifstream::eofbit | ifstream::badbit);
+    dbs.open(name);
 }
 
 int main()
 {
     cout << "Hello, world!" << endl;
-	try {
-		readDbs("C:\\geodet\\ring.DBS");
-	}
-	catch(exception &e){
-		cerr << "Exception: " << e.what() << endl;
-	}
-	catch(...) {
-		cerr << "Unknown exception!" << endl;
-	}
+    try {
+        readDbs("C:\\geodet\\ring.DBS");
+    }
+    catch(exception &e){
+        cerr << "Exception: " << e.what() << endl;
+    }
+    catch(...) {
+        cerr << "Unknown exception!" << endl;
+    }
     return 0;
 }
 
