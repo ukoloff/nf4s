a10678679ef42d62b9b49b11b9a5f6a533d34059
Stas Ukolov <ukoloff@gmail.com>
2016-11-22 14:35:14 +0500

    Other DBS records

--- nf4s/iDbs.h
+++ nf4s/iDbs.h
@@ -5,6 +5,7 @@ static_assert(2 == sizeof(short), "Invalid short int!");
 namespace dbs {
     namespace i
     {
+        // Base record
         struct Base
         {
             short length;
@@ -20,20 +21,44 @@ namespace dbs {
             }
         };
 
+        // Minimal non-trivial record
         struct Rec : Base
         {
             short _01, length2, _02, kind, _03;
         };
 
-        struct alignas(4) Rid : Rec
+        // Record with ID
+        struct Rid : Rec
         {
             short id, _04;
         };
 
-        struct alignas(4) R1 : Rid
+        // Record 1: geometry
+        struct R1 : Rid
         {
             short subType, _05, isText, _06, isAuto, _07, group, _08, orig, _09, rev, _0A;
             dbs::O2 o2;
+            dbs::Node nodes[];
+        };
+
+        // Record 8: details' contours
+        struct R8 : Rid
+        {
+            struct{
+                short id, _;
+            } ids[];
+        };
+
+        // Record 26: detail name
+        struct R26 : Rid
+        {
+            char name[8];
+        };
+
+        // Record 27: detail area & perimeter
+        struct R27 : Rid
+        {
+            float S, P;
         };
     }
 }
