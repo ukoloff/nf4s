0788364a529b6c6bf16fec56449a1fff58d6e4b7
Stas Ukolov <ukoloff@gmail.com>
2016-11-26 12:53:47 +0500

    Use command line

--- nf4s/nf4s.cpp
+++ nf4s/nf4s.cpp
@@ -6,12 +6,17 @@
 #include "stdafx.h"
 #include "Dbs.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(2 != argc)
+    {
+        cerr << "Usage: " << argv[0] << " filename.dbs\n";
+        return 1;
+    }
     try 
     {
         dbs::File f;
-        f.read("C:\\geodet\\ring.DBS");
+        f.read(argv[1]);
         f.yaml(cout);
         // throw new dbs::Error("Oops");
     }
