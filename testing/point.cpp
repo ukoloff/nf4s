fda9e3d6ad2d80eb14e9314d22f213f4ca72f7b5
Stas Ukolov <ukoloff@gmail.com>
2016-11-27 20:48:58 +0500

    Test dbs::P

---
+++ testing/point.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "Dbs.h"
+
+using namespace dbs;
+
+TEST(point, to_c)
+{
+    P p = {1, 2};
+    ASSERT_EQ(p.to_c(), Complex(1, 2));
+}
--- testing/test.cpp
+++ testing/test.cpp
@@ -1,5 +1,4 @@
 #include "gtest/gtest.h"
-#include "Dbs.h"
 
 TEST(dummy, equal)
 {
