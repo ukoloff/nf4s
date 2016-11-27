37cfb4ca711a3e919ca0ff27270365e31759528b
Stas Ukolov <ukoloff@gmail.com>
2016-11-27 14:42:37 +0500

    More docs

--- dbs/span.cpp
+++ dbs/span.cpp
@@ -24,12 +24,35 @@ const Complex dbs::Span::center() const
     return linear(Complex(0, 1 / bulge - bulge));
 }
 
+/** \brief Find point on the arc with more uniform distribution (compared to [])
+ *
+ * \param pos float
+ * \return const Complex
+ *
+ * Maps -1 to arc's begin, 0 to middle of arc, +1 to ark's end.
+ *
+ * Other points from [-1, +1] are mapped to arc's points.
+ *
+ * Resulting distribution is as much uniform as possible,
+ * whereas []'s points tend to concentrate near arc's ends
+ * (for big bulge values).
+ */
 const Complex dbs::Span::at(float pos) const
 {
     auto q = sqrt(1 + bulge * bulge);
     return (*this)[pos / (q - (q-1) * abs(bulge))];
 }
 
+/** \brief Get real coordinates of point around arc
+ *
+ * \param pos
+ *  Complex Source point in abstract space
+ * \return const Complex
+ *  Destination points in arc's space
+ *
+ * Maps -1 to Arc's begin, +1 to Arc's end,
+ * otherwise perform linear transformation.
+ */
 const Complex dbs::Span::linear(Complex pos) const
 {
   return (to_c() * pos + a.to_c() + b.to_c()) / (float)2;
@@ -49,6 +72,7 @@ const Complex dbs::Span::zenith() const
  *
  * \return const Complex
  *
+ * This points is not on the arc, it is far in space on another side...
  */
 const Complex dbs::Span::nadir() const
 {
