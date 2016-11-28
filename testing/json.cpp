7b9b7ec59e969c34a2098594bbafc34cae599504
Stas Ukolov <ukoloff@gmail.com>
2016-11-28 18:24:24 +0500

    Fix string syntax

--- testing/json.cpp
+++ testing/json.cpp
@@ -6,6 +6,7 @@ using namespace dbs;
 TEST_CASE("JSON string encode")
 {
     REQUIRE(Part::quote("qwe") == R"("qwe")");
-    REQUIRE(Part::quote(R"(as"df\zx)") == R"("as\"df\\zx")");
+    const string dst = R"("as\"df\\zx")";
+    REQUIRE(Part::quote("as\"df\\zx") == dst);
     REQUIRE(Part::quote("-\r-\n-") == R"("-\r-\n-")");
 }
