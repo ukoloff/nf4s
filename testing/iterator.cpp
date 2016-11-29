123e64c264d3e64e0f0f4764f65d04084b4939ed
Stas Ukolov <ukoloff@gmail.com>
2016-11-29 23:29:36 +0500

    Closer look to iterator

--- testing/iterator.cpp
+++ testing/iterator.cpp
@@ -10,8 +10,23 @@ TEST_CASE("Click iterator")
     REQUIRE(Z.parts.size() == 1);
     REQUIRE(Z.parts[0].paths.size() == 1);
     iSpan i(Z.parts[0].paths[0]);
-    size_t n = 0;
-    while(auto span = i.get())
-        n++;
-    REQUIRE(n == 8);
+
+    SECTION("Get size")
+    {
+      size_t n = 0;
+      while(auto span = i.get())
+          n++;
+      REQUIRE(n == 8);
+      REQUIRE(n + 1 == Z.parts[0].paths[0].nodes.size());
+    }
+
+    SECTION("Test ordering")
+    {
+      size_t n = 0;
+      while(auto span = i.get())
+      {
+          REQUIRE(span->a == Z.parts[0].paths[0].nodes[n].to_p());
+          REQUIRE(span->b == Z.parts[0].paths[0].nodes[++n].to_p());
+      }
+    }
 }
