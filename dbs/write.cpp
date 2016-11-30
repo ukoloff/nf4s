2c0d4222f107a13304e144de1dfacb4bbb99f738
Stas Ukolov <ukoloff@gmail.com>
2016-11-30 18:27:06 +0500

    Add write implementation seed

--- dbs/write.cpp
+++ dbs/write.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "iDbs.h"
 
+/** \brief Get ready for writing to disk
+ *
+ * \return void
+ *
+ * Set some internal fields to be written correctly.
+ */
 void dbs::i::Rec::on_write()
 {
     _01 = id;
