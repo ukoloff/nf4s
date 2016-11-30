6102fc0427bce9ad9a7d3c30e421213407a9b7d6
Stas Ukolov <ukoloff@gmail.com>
2016-11-30 23:10:57 +0500

    Optimize iterator test

--- testing/iterator.cpp
+++ testing/iterator.cpp
@@ -7,15 +7,19 @@ TEST_CASE("Click iterator")
 {
     File Z;
     Z.read("../testing/geodet/rounded3x4.dbs");
-    REQUIRE(Z.parts.size() == 1);
-    REQUIRE(Z.parts[0].paths.size() == 1);
-    iSpan i(Z.parts[0].paths[0]);
+
+    SECTION("Check counts")
+    {
+      REQUIRE(Z.parts.size() == 1);
+      REQUIRE(Z.parts[0].paths.size() == 1);
+    }
+
+    auto path = Z.parts[0].paths[0];
 
     SECTION("Get size")
     {
       size_t n = 0;
-      while(auto span = i.get())
-          n++;
+      for(auto i = path.spans(); auto span = i.get(); n++);
       REQUIRE(n == 8);
       REQUIRE(n + 1 == Z.parts[0].paths[0].nodes.size());
     }
@@ -23,7 +27,7 @@ TEST_CASE("Click iterator")
     SECTION("Test ordering")
     {
       size_t n = 0;
-      while(auto span = i.get())
+      for(auto i = path.spans(); auto span = i.get(); )
       {
           REQUIRE(span->a == Z.parts[0].paths[0].nodes[n].to_p());
           REQUIRE(span->b == Z.parts[0].paths[0].nodes[++n].to_p());
