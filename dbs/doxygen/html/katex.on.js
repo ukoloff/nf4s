3d203ec6b45d41458b544bc097158d119720b7f3
Stas Ukolov <ukoloff@gmail.com>
2016-11-30 17:26:50 +0500

    Invoke KaTeX

--- dbs/doxygen/html/katex.on.js
+++ dbs/doxygen/html/katex.on.js
@@ -1,3 +1,21 @@
 $(function(){
 
+$('img.formulaInl').each(function(){
+  embed(this, false);
+})
+
+$('img.formulaDsp').each(function(){
+  embed(this, true);
+})
+
+function embed(el, standalone)
+{
+  var skip = standalone ? 2 : 1;
+  var formula = el.alt.substring(skip);
+  formula = formula.substring(0, formula.length - skip);
+  formula = katex.renderToString(formula, {displayMode: standalone});
+  console.log(standalone, formula);
+  $(el).wrap('<span>').parent().html(formula);
+}
+
 });
