ad8d2569a552730c5240c7aad8df82264ce218f1
Stas Ukolov <ukoloff@gmail.com>
2017-01-20 12:46:42 +0500

    Add dbs2json project

---
+++ dbs2json/!stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+//#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+//== Project's
+
+#include <complex>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <map>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
