602937d4edc5b330777277b506ea325d35152fc5
Stas Ukolov <ukoloff@gmail.com>
2017-01-23 14:22:45 +0500

    Format SVG

--- dbs2json/dbs2json.cpp
+++ dbs2json/dbs2json.cpp
@@ -1,6 +1,7 @@
 #include "!stdafx.h"
 #include "Dbs.h"
 
+static void SVG(const dbs::File &);
 
 int main(int argc, char *argv[])
 {
@@ -25,7 +26,7 @@ int main(int argc, char *argv[])
             f.dxf(cout);
             break;
         case 's':
-            f.svg(cout);
+            SVG(f);
             break;
         default:
             f.json(cout);
@@ -40,4 +41,48 @@ int main(int argc, char *argv[])
         cerr << "Unknown exception!" << endl;
     }
     return 0;
+}
+
+static const std::string Preamble = R"HTML(
+<html>
+<head>
+<style>
+body {
+  margin: 0;
+}
+svg {
+  x-border: 1px dotted red;
+  box-sizing: border-box;
+}
+path {
+  fill: lime;
+  stroke: red;
+  stroke-width: 1;
+  fill-rule: nonzero;
+  stroke-linejoin: round;
+}
+</style>
+</head>
+<body>
+<svg height="100%" width="100%" viewBox=")HTML";
+
+static const std::string Middle = R"HTML("><g transform = "scale(1, 1)">
+)HTML";
+
+static const std::string Postamble = R"HTML(
+</g>
+</svg>
+</body>
+</html>
+)HTML";
+
+static void SVG(const dbs::File & dbs)
+{
+    cout << Preamble;
+    auto b = dbs.bounds();
+    auto sz = b.max.to_c() - b.min.to_c();
+    cout << b.min.x << " " << b.min.y << " " << sz.real() << " " << sz.imag();
+    cout << Middle;
+    dbs.svg(cout);
+    cout << Postamble;
 }
